3511896d0ce8a2ab00a547c953a2464558839646
kenny <sphy@foxmail.com>
2015-10-30 11:55:56 +0800

    fix up child clone/pull may undone when main process ends

--- gitcmd/__init__.py
+++ gitcmd/__init__.py
@@ -58,10 +58,10 @@ class GitCmd(object):
         if os.path.exists(self.work_dir):
             raise Exception("git repo's dir already exist")
         pexpect.run("mkdir -p %s" % self.work_dir)
-        self.wait_transfer_end(self.execute('git clone %s .' % self.url, wait=False), timeout)
+        return self.wait_transfer_end(self.execute('git clone %s .' % self.url, wait=False), timeout)
 
     def pull(self, timeout=600):
-        self.wait_transfer_end(self.execute(cmd="git pull", wait=False), timeout)
+        return self.wait_transfer_end(self.execute(cmd="git pull", wait=False), timeout)
 
     def execute(self, cmd="git status", wait=True, timeout=60):
         self.need_work_dir()
@@ -69,6 +69,7 @@ class GitCmd(object):
             child = self.auth_if_need(pexpect.spawn(cmd))
             if self.debug:
                 child.logfile = sys.stdout
+                sys.stdout.write("\t\t>>>>Execute: %s, wait=%s,timeout=%s\n" % (cmd, wait, timeout))
             if wait:
                 self.wait(child, timeout)
             return child
@@ -84,6 +85,14 @@ class GitCmd(object):
         if cc and commit_id in ['master', cc]:
             return True
         else:
+            return False
+
+    def reset(self, commit_id="", timeout=10):
+        ch = self.execute("git reset --hard %s" % commit_id, wait=False)
+        try:
+            ch.expect('HEAD is now .*', timeout=timeout)
+            return True
+        except:
             return False
 
     def wait(self, ch, timeout):
@@ -103,13 +112,25 @@ class GitCmd(object):
         ch.expect(pexpect.EOF, timeout=timeout)
 
     def current_commit(self):
-        with working_directory(self.work_dir):
-            return os.popen('git rev-parse HEAD').read().strip()
+        return os.popen('cd %s && git rev-parse HEAD' % self.work_dir).read().strip()
 
     def wait_transfer_end(self, ch, timeout):
-        ch.expect(['Checking connectivity... done.', 'Already up-to-date.\r\n', pexpect.EOF], timeout=timeout)
+        flag = ch.expect(['Checking connectivity... done.', 'Already up-to-date.\r\n', 'fatal: [\S\s]*', pexpect.EOF, ],
+                         timeout=timeout)
+        print ">>>>>FLAG", flag
+        if flag in (0, 1):
+            self.wait(ch, timeout)
+            return ch
+        else:
+            return False
 
 
 class TimeoutError(Exception):
     def __init__(self, timeout):
         super(TimeoutError, self).__init__("TimeoutError(%ds)" % timeout)
+
+
+class CmdExecuteError(Exception):
+    def __init__(self, ch):
+        msg = 'Args:%s,Closed:%s,ExitCode:%s' % (ch.args, ch.closed, ch.exitstatus)
+        super(CmdExecuteError, self).__init__(msg)
